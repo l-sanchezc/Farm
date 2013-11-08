0e2983141422491b5e326d185512b5848a6c5280
Lázaro Sánchez <l.sanchezc@alumnos.upm.es>
2013-11-08 12:44:30 +0100

    Initial Commit

---
+++ Farm/FarmViewController.h
@@ -0,0 +1,13 @@
+//
+//  FarmViewController.h
+//  Farm
+//
+//  Created by Lázaro Sánchez Campos on 08/11/13.
+//  Copyright (c) 2013 UPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FarmViewController : UIViewController
+
+@end
---
+++ Farm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Farm
+//
+//  Created by Lázaro Sánchez Campos on 08/11/13.
+//  Copyright (c) 2013 UPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FarmAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FarmAppDelegate class]));
+    }
+}
